d33cc740a34f3a6c2b77f6159070e83155806caf
Peter Hevesi <peter.hevesi@dfki.de>
2018-11-12 17:05:40 +0100

    Implement features for mongoDB access

--- models.go
+++ models.go
@@ -1,3 +1,12 @@
 package main
 
+import "github.com/mongodb/mongo-go-driver/bson/objectid"
+
 // TODO: use this file to define data structs and models used across the main package
+type User struct {
+	ID             objectid.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	Email          string            `bson:"email" json:"email"`
+	EmailConfirmed bool              `bson:"email_confirmed" json:"email_confirmed"`
+	Password       string            `bson:"password" json:"password"`
+	Roles          []string          `bson:"roles" json:"roles"`
+}
