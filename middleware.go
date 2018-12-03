92c450049afb68cec3635eb8223edc2354de074a
Fabian Süß <despia@users.noreply.github.com>
2018-12-03 11:03:35 +0100

    middleware to read user from payload

---
+++ middleware.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func bindUserFromBodyMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var user User
+
+		if c.Request.ContentLength == 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "payload missing"})
+			c.Abort()
+			return
+		}
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+
+		c.Set("user", user)
+
+		c.Next()
+	}
+}
