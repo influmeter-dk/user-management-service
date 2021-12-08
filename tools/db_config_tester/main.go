c2eec49bc785498748ec79a85f7a9e3a87251597
Kevin Brendel <kBrendel@hotmail.de>
2021-12-09 00:53:31 +0100

    Added benchmark.

--- tools/db_config_tester/main.go
+++ tools/db_config_tester/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
@@ -17,7 +18,14 @@ import (
 
 const INSTANCE_ID = "v1"
 
+var userFilter = userdb.UserFilter{
+	OnlyConfirmed:   false,
+	ReminderWeekDay: -1,
+}
+
 var userDB *userdb.UserDBService
+
+var benchmarkTimes []int64
 
 func init() {
 	conf := getDBConfig()
@@ -28,43 +36,27 @@ func main() {
 	parseFlags()
 }
 
-func getDBConfig() models.DBConfig {
-	connStr := os.Getenv("USER_DB_CONNECTION_STR")
-	username := os.Getenv("USER_DB_USERNAME")
-	password := os.Getenv("USER_DB_PASSWORD")
-	prefix := os.Getenv("USER_DB_CONNECTION_PREFIX") // Used in test mode
-	if connStr == "" || username == "" || password == "" {
-		logger.Error.Fatal("Couldn't read DB credentials.")
-	}
-	URI := fmt.Sprintf(`mongodb%s://%s:%s@%s`, prefix, username, password, connStr)
+func parseFlags() {
+	usersToGenerate := flag.Int("generateUsers", -1, "How many users to generate.")
+	runBenchmark := flag.Bool("benchmark", false, "Whether to run the benchmark.")
+	flag.Parse()
 
-	var err error
-	Timeout, err := strconv.Atoi(os.Getenv("DB_TIMEOUT"))
-	if err != nil {
-		logger.Error.Fatal("DB_TIMEOUT: " + err.Error())
+	if *usersToGenerate > 0 {
+		generateUsers(*usersToGenerate)
 	}
-	IdleConnTimeout, err := strconv.Atoi(os.Getenv("DB_IDLE_CONN_TIMEOUT"))
-	if err != nil {
-		logger.Error.Fatal("DB_IDLE_CONN_TIMEOUT" + err.Error())
+	if *runBenchmark {
+		benchmark()
 	}
-	mps, err := strconv.Atoi(os.Getenv("DB_MAX_POOL_SIZE"))
-	MaxPoolSize := uint64(mps)
-	if err != nil {
-		logger.Error.Fatal("DB_MAX_POOL_SIZE: " + err.Error())
-	}
+}
 
-	noCursorTimeout := os.Getenv("ENV_USE_NO_CURSOR_TIMEOUT") == "true"
-
-	DBNamePrefix := os.Getenv("DB_DB_NAME_PREFIX")
-
-	return models.DBConfig{
-		URI:             URI,
-		Timeout:         Timeout,
-		IdleConnTimeout: IdleConnTimeout,
-		NoCursorTimeout: noCursorTimeout,
-		MaxPoolSize:     MaxPoolSize,
-		DBNamePrefix:    DBNamePrefix,
+func generateUsers(usersToGenerate int) {
+	for a := 0; a < usersToGenerate; a++ {
+		_, err := userDB.AddUser(INSTANCE_ID, generateRandomUser())
+		if err != nil {
+			logger.Error.Fatal(err)
+		}
 	}
+	logger.Info.Printf("%v users generated", usersToGenerate)
 }
 
 func generateRandomUser() models.User {
@@ -92,21 +84,91 @@ func generateRandomUser() models.User {
 	}
 }
 
-func generateUsers(usersToGenerate int) {
-	for a := 0; a < usersToGenerate; a++ {
-		_, err := userDB.AddUser(INSTANCE_ID, generateRandomUser())
-		if err != nil {
-			logger.Error.Fatal(err)
-		}
+func benchmark() {
+	benchmarkTimes = nil
+	recordBenchmarkTime()
+
+	err := userDB.PerfomActionForUsers(INSTANCE_ID, userFilter, benchmarkCallback)
+	if err != nil {
+		logger.Error.Printf(err.Error())
 	}
-	logger.Info.Printf("%v users generated", usersToGenerate)
+
+	saveBenchmarkTimesCSV()
 }
 
-func parseFlags() {
-	usersToGenerate := flag.Int("generateUsers", -1, "How many users to generate.")
-	flag.Parse()
+func benchmarkCallback(instanceID string, user models.User, args ...interface{}) error {
+	recordBenchmarkTime()
+	return nil
+}
 
-	if *usersToGenerate > 0 {
-		generateUsers(*usersToGenerate)
+func recordBenchmarkTime() {
+	benchmarkTimes = append(benchmarkTimes, time.Now().UnixNano())
+}
+
+func saveBenchmarkTimesCSV() {
+	file, err := os.OpenFile("./benchmark.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		logger.Error.Fatalf("failed creating file: %s", err)
+	}
+
+	file.Truncate(0)
+	file.Seek(0, 0)
+
+	datawriter := bufio.NewWriter(file)
+	datawriter.WriteString("Timestamp, Delta\n")
+
+	for i, data := range benchmarkTimes {
+		var reference int64
+
+		if i > 0 {
+			reference = benchmarkTimes[i-1]
+		} else {
+			reference = data
+		}
+
+		_, _ = datawriter.WriteString(strconv.FormatInt(data, 10) + "," + strconv.FormatInt(data-reference, 10) + "\n")
+	}
+
+	datawriter.Flush()
+	file.Close()
+}
+
+func getDBConfig() models.DBConfig {
+	connStr := os.Getenv("USER_DB_CONNECTION_STR")
+	username := os.Getenv("USER_DB_USERNAME")
+	password := os.Getenv("USER_DB_PASSWORD")
+	prefix := os.Getenv("USER_DB_CONNECTION_PREFIX") // Used in test mode
+	if connStr == "" || username == "" || password == "" {
+		logger.Error.Fatal("Couldn't read DB credentials.")
+	}
+	URI := fmt.Sprintf(`mongodb%s://%s:%s@%s`, prefix, username, password, connStr)
+
+	var err error
+	Timeout, err := strconv.Atoi(os.Getenv("DB_TIMEOUT"))
+	if err != nil {
+		logger.Error.Fatal("DB_TIMEOUT: " + err.Error())
+	}
+	IdleConnTimeout, err := strconv.Atoi(os.Getenv("DB_IDLE_CONN_TIMEOUT"))
+	if err != nil {
+		logger.Error.Fatal("DB_IDLE_CONN_TIMEOUT" + err.Error())
+	}
+	mps, err := strconv.Atoi(os.Getenv("DB_MAX_POOL_SIZE"))
+	MaxPoolSize := uint64(mps)
+	if err != nil {
+		logger.Error.Fatal("DB_MAX_POOL_SIZE: " + err.Error())
+	}
+
+	noCursorTimeout := os.Getenv("USE_NO_CURSOR_TIMEOUT") == "true"
+
+	DBNamePrefix := os.Getenv("DB_DB_NAME_PREFIX")
+
+	return models.DBConfig{
+		URI:             URI,
+		Timeout:         Timeout,
+		IdleConnTimeout: IdleConnTimeout,
+		NoCursorTimeout: noCursorTimeout,
+		MaxPoolSize:     MaxPoolSize,
+		DBNamePrefix:    DBNamePrefix,
 	}
 }
